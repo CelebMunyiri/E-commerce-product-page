5b7b49c18e9d98bc37c413f82c03b421fa0a2c13
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-30 19:57:15 +0300

    added all the functionalities in both buttons

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-const images=['./images/image-product-2-thumbnail.jpg','./images/image-product-3-thumbnail.jpg','./images/image-product-4-thumbnail.jpg']
+const images=['./images/image-product-1-thumbnail.jpg','./images/image-product-2-thumbnail.jpg','./images/image-product-3-thumbnail.jpg','./images/image-product-4-thumbnail.jpg']
 const previous=document.querySelector('.previous')
 const next=document.querySelector('.nextimg')
 let imageProduct=document.querySelector('.imgProduct')
@@ -8,10 +8,16 @@ let currentImage=0;
 next.addEventListener('click',()=>{
     currentImage=currentImage+1;
     imageProduct.src=images[currentImage];
-    if(currentImage==images.length){
-        let currentImage=0
+    if(currentImage>=images.length-1){
+        currentImage=0
+        
+    }    
+})
+
+previous.addEventListener('click',()=>{
+    currentImage=currentImage-1
+    imageProduct.src=images[currentImage]
+    if(currentImage<=0){
+        currentImage=images.length-1
     }
-    
-   
-    
 })
